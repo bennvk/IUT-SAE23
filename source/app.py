0d6e5c1f3dab2f895008505ec7b805fedcc36b7f
bennvk <bne.ppn@gmail.com>
2025-05-23 19:55:03 +0200

    Update app.py

--- source/app.py
+++ source/app.py
@@ -1,55 +1,111 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ton_user:ton_mdp@localhost:3306/nom_de_ta_base'
+app.secret_key = 'secret_key_change_this'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:progtr00@localhost/skills_database'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+##############################
+#####      DATABASE      #####
+##############################
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return f'Bonjour, {session["username"]}! <a href="/logout">Se déconnecter</a>'
-    return render_template('~/Documents/IUT-SAE23/code/index.html')
+class Semestre(db.Model):
+    __tablename__ = 'semestres'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(50), unique=True, nullable=False)
+    nom = db.Column(db.String(100), nullable=False)
+    blocs = db.relationship('Bloc', backref='semestre', cascade="all, delete")
+
+class Bloc(db.Model):
+    __tablename__ = 'blocs'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(50), unique=True, nullable=False)
+    nom = db.Column(db.String(100), nullable=False)
+    semestre_id = db.Column(db.Integer, db.ForeignKey('semestres.id'), nullable=False)
+    competences = db.relationship('Competence', backref='bloc', cascade="all, delete")
+
+class Competence(db.Model):
+    __tablename__ = 'competences'
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(50), unique=True, nullable=False)
+    nom = db.Column(db.String(255), nullable=False)
+    bloc_id = db.Column(db.Integer, db.ForeignKey('blocs.id'), nullable=False)
+    niveaux = db.relationship('CompetenceNiveau', backref='competence', cascade="all, delete")
+
+class Niveau(db.Model):
+    __tablename__ = 'niveaux'
+    id = db.Column(db.Integer, primary_key=True)
+    niveau = db.Column(db.String(50), unique=True, nullable=False)
+    competences = db.relationship('CompetenceNiveau', backref='niveau')
+
+class CompetenceNiveau(db.Model):
+    __tablename__ = 'competences_niveaux'
+    id = db.Column(db.Integer, primary_key=True)
+    competence_id = db.Column(db.Integer, db.ForeignKey('competences.id'), nullable=False)
+    niveau_id = db.Column(db.Integer, db.ForeignKey('niveaux.id'), nullable=False)
+
+##############################
+#####       ROUTES       #####
+##############################
+
+def home():
+    return render_template('home.html')
+
+@app.route('/skills')
+def skills():
+    data = db.session.query(Competence, Bloc, Semestre, Niveau).\
+        join(Bloc).join(Semestre).\
+        outerjoin(CompetenceNiveau).outerjoin(Niveau).all()
+    return render_template('skills.html', data=data)
+
+@app.route('/add-skill', methods=['GET', 'POST'])
+def add_skill():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    blocs = Bloc.query.all()
+    niveaux = Niveau.query.all()
+
+    if request.method == 'POST':
+        code = request.form['code']
+        nom = request.form['nom']
+        bloc_id = request.form['bloc_id']
+        niveau_id = request.form['niveau_id']
+
+        comp = Competence(code=code, nom=nom, bloc_id=bloc_id)
+        db.session.add(comp)
+        db.session.commit()
+
+        comp_niv = CompetenceNiveau(competence_id=comp.id, niveau_id=niveau_id)
+        db.session.add(comp_niv)
+        db.session.commit()
+
+        return redirect(url_for('skills'))
+
+    return render_template('add_skill.html', blocs=blocs, niveaux=niveaux)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username']).first()
-        if user and check_password_hash(user.password, request.form['password']):
-            session['username'] = user.username
-            return redirect(url_for('index'))
-        return 'Identifiants invalides'
-    return render_template('~/Documents/IUT-SAE23/code/login.html')
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            return "Nom d'utilisateur ou mot de passe incorrect"
+    return render_template('login.html')
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
-    return redirect(url_for('~/Documents/IUT-SAE23/code/index'))
+    session.pop('logged_in', None)
+    return redirect(url_for('home'))
 
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        hashed_password = generate_password_hash(request.form['password'], method='sha256')
-        new_user = User(username=request.form['username'], password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('~/Documents/IUT-SAE23/code/register.html')
-
-def create_tables():
-    db.create_all()
+##############################
+#####      APP RUN       #####
+##############################
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
